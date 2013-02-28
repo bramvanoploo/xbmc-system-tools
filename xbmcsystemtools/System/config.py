15fab0cd7f0837147ecd4a91a7351f93b9472a28
Bram77 <bram@sudo-systems.com>
2013-02-28 19:12:23 +0100

    Using absolute paths everywhere now

--- xbmcsystemtools/System/config.py
+++ xbmcsystemtools/System/config.py
@@ -19,7 +19,7 @@ xbmc_home_dir = home_directory+ ".xbmc/"
 xbmc_addons_dir = xbmc_home_dir+ "addons/"
 xbmc_userdata_dir = xbmc_home_dir+ "userdata/"
 xbmc_keymaps_dir = xbmc_home_dir+ "addons/"
-xbmc_backups_dir = 'static/backups/'
+xbmc_backups_dir = installation_path+'static/backups/'
 xbmc_backups_url_path = 'backups/'
 
 xbmc_initd_file = "/etc/init.d/xbmc"
