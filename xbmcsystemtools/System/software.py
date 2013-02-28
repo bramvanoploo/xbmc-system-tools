7d12d944fde066447b54c176ea7ff025e6e4433c
Bram77 <bram@sudo-systems.com>
2013-02-28 18:39:07 +0100

    Fixed some weird error in software.py (cannot close cache)

--- xbmcsystemtools/System/software.py
+++ xbmcsystemtools/System/software.py
@@ -9,10 +9,11 @@ def update_sources():
         apt_cache = apt.cache.Cache()
         apt_cache.update()
         success = apt_cache.commit(apt.progress.TextFetchProgress(), apt.progress.InstallProgress())
-        apt_cache.close()
+        #apt_cache.close()
         return success
     except AttributeError as e:
         log.error('AttributeError: ' +str(e), stack()[0][3])
+        #apt_cache.close()
         return False
 
 def dist_upgrade():
@@ -21,7 +22,7 @@ def dist_upgrade():
     apt_cache.open(None)
     apt_cache.upgrade(True)
     success = apt_cache.commit(apt.progress.TextFetchProgress(), apt.progress.InstallProgress())
-    apt_cache.close()
+    #apt_cache.close()
     return success
 
 def install(package_name):
@@ -33,21 +34,21 @@ def install(package_name):
         if pkg.isInstalled:
             apt_pkg.PkgSystemUnLock()
             log.info('Trying to install a package that is already installed (' +package_name.strip()+ ')', stack()[0][3])
-            apt_cache.close()
+            #apt_cache.close()
             return False
         else:
             pkg.mark_install()
             try:
                 apt_pkg.PkgSystemUnLock()
                 result = apt_cache.commit()
-                apt_cache.close()
+                #apt_cache.close()
                 return result
             except SystemError as e:
                 log.error('SystemError: ' +str(e), stack()[0][3])
-                apt_cache.close()
+                #apt_cache.close()
                 return False
     else:
-        apt_cache.close()
+        #apt_cache.close()
         log.error('Unknown package selected (' +package_name.strip()+ ')', stack()[0][3])
         return False
 
@@ -66,14 +67,14 @@ def remove(package_name, purge = False):
             try:
                 apt_pkg.PkgSystemUnLock()
                 result = apt_cache.commit()
-                apt_cache.close()
+                #apt_cache.close()
                 return result
             except SystemError as e:
                 log.error('SystemError: ' +str(e), stack()[0][3])
-                apt_cache.close()
+                #apt_cache.close()
                 return False
     else:
-        apt_cache.close()
+        #apt_cache.close()
         log.info('Unknown package selected (' +package_name.strip()+ ')', stack()[0][3])
         return False
 
@@ -85,12 +86,13 @@ def auto_remove():
 
 def search(package_name, installed_packes):
     apt_cache = apt.cache.Cache()
+    apt_cache.open(None)
     packages = apt_cache.keys()
     if installed_packes:
         result = [value for value in packages if apt_cache[value].isInstalled and package_name.strip() in value]
     else:
         result = [value for value in packages if not apt_cache[value].isInstalled and package_name.strip() in value]
-    apt_cache.close()
+    #apt_cache.close()
     return sorted(result)
 
 def is_package_installed(package_name):
@@ -99,7 +101,7 @@ def is_package_installed(package_name):
     if package_name.strip() in cache and cache[package_name.strip()].is_installed:
         apt_cache.close()
         return True
-    apt_cache.close()
+    #apt_cache.close()
     return False
 
 def package_exists(package_name):
@@ -108,5 +110,5 @@ def package_exists(package_name):
     if package_name.strip() in cache:
         apt_cache.close()
         return True
-    apt_cache.close()
+    #apt_cache.close()
     return False
