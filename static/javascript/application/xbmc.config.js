4bb9a7ffcb6b1237ac658cce5159753c3f8593ae
Bram77 <bram@sudo-systems.com>
2013-03-19 20:45:01 +0100

    Building XBMC repository management functionality (WIP)

--- static/javascript/application/xbmc.config.js
+++ static/javascript/application/xbmc.config.js
@@ -1,13 +1,13 @@
-function Xbmc() {
-    this.repositories = function() {
+function XbmcConfig() {
+    this.getAvailableRepositories = function() {
         return new Array(
-            [data.length] = new Array('Official stable builds', 'ppa:team-xbmc/ppa'),
-            [data.length] = new Array('Official unstable builds', 'ppa:team-xbmc/unstable'), 
-            [data.length] = new Array('Official nightly builds', 'ppa:team-xbmc/xbmc-nightly'),
-            [data.length] = new Array('Wsnipex nightly builds', 'ppa:wsnipex/xbmc-nightly'),
-            [data.length] = new Array('Wsnipex xvba frodo builds', 'ppa:wsnipex/xbmc-xvba-frodo'),
-            [data.length] = new Array('Wsnipex xvba stable regular builds', 'ppa:wsnipex/xbmc-xvba'),
-            [data.length] = new Array('Wsnipex xvba unstable builds', 'ppa:wsnipex/xbmc-xvba-testing'),
+            new Array('Official stable builds', 'ppa:team-xbmc/ppa'),
+            new Array('Official unstable builds', 'ppa:team-xbmc/unstable'), 
+            new Array('Official nightly builds', 'ppa:team-xbmc/xbmc-nightly'),
+            new Array('Wsnipex nightly builds', 'ppa:wsnipex/xbmc-nightly'),
+            new Array('Wsnipex xvba frodo builds', 'ppa:wsnipex/xbmc-xvba-frodo'),
+            new Array('Wsnipex xvba stable regular builds', 'ppa:wsnipex/xbmc-xvba'),
+            new Array('Wsnipex xvba unstable builds', 'ppa:wsnipex/xbmc-xvba-testing')
         );
     }
 }
